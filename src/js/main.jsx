2cbd45a122dac15d07e8eda1dddf7123061c4495
SashaGiT <sashaotero5@gmail.com>
2025-10-28 20:15:15 +0000

    Css Actualizado

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -3,9 +3,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
+import "../styles/index.css";
 
-const container = document.getElementById("root"); 
-if (!container) throw new Error("No se encontró el contenedor #root en index.html");
+const container = document.getElementById("root");
+if (!container) throw new Error("No se encontró el contenedor raíz");
 
 ReactDOM.createRoot(container).render(
   <React.StrictMode>
